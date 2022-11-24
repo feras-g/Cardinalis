6ef406f7e69949f676c2a0804d715ddafc73a69b
embooo <79663490+embooo@users.noreply.github.com>
2022-11-24 20:26:36 +0100

    Add helper funciton to chose an allocation memory type

--- src/core/Rendering/Vulkan/VulkanTools.h
+++ src/core/Rendering/Vulkan/VulkanTools.h
@@ -1,14 +1,15 @@
-#pragma once 
+#ifndef VULKAN_TOOLS_H
+#define VULKAN_TOOLS_H
 
 #include <string>
 #include <sstream>
 #include "Core/EngineLogger.h"
+#include "Rendering/Vulkan/VulkanRenderInterface.h"
 
 static constexpr uint64_t OneSecondInNanoSeconds = 1000000000;
 
 #define VK_CHECK(x)													                                \
 if(x != VK_SUCCESS) { LOG_ERROR("Assert on VkResult : {0}", string_VkResult(x)); assert(false); };  \
-
 
 #define GET_INSTANCE_PROC_ADDR(inst, entry)                            \
 {                                                                      \
@@ -23,3 +24,21 @@ if(x != VK_SUCCESS) { LOG_ERROR("Assert on VkResult : {0}", string_VkResult(x));
   if (!fp##entry)                                                   \
     EXIT_ON_ERROR("vkGetDeviceProcAddr failed to find vk" #entry);  \
 }
+
+static uint32_t FindMemoryType(VkPhysicalDevice physDevice, uint32_t memoryTypeBits, VkMemoryPropertyFlags reqMemoryProperties)
+{
+    // Query available types of memory
+    VkPhysicalDeviceMemoryProperties deviceMemProperties;
+    vkGetPhysicalDeviceMemoryProperties(physDevice, &deviceMemProperties);
+
+    // Find the most suitable memory type 
+    for (uint32_t i = 0; i < deviceMemProperties.memoryTypeCount; i++)
+    {
+        if ((memoryTypeBits & (1 << i) && (deviceMemProperties.memoryTypes[i].propertyFlags & reqMemoryProperties) == reqMemoryProperties))
+        {
+            return i;
+        }
+    }
+}
+
+#endif // !VULKAN_TOOLS_H
