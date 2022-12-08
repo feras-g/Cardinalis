fc26099e6711b04ba1c0a4fa374983a38fef679c
embooo <79663490+embooo@users.noreply.github.com>
2022-12-08 19:48:41 +0100

    Modify ImGui path

--- premake5.lua
+++ premake5.lua
@@ -7,7 +7,7 @@ workspace "Cardinalis"
 	premake_dir = "premake/scripts/"
 	outputdir = "%{cfg.buildcfg}-%{prj.name}-%{cfg.system}-%{cfg.architecture}" -- ex: Debug-Windows-x64
 
-	include("src/thirdparty/imgui/imgui/examples/premake5-lib.lua") -- ImGui
+	include("src/thirdparty/imgui/examples/premake5-lib.lua") -- ImGui
 
 -- Generate projects based on a list of scripts
 	scripts = 
